e18a82e4d19f56156d852777cb250c9b2c3cf8ea
slightlightss66-stack <slightlightss66@gmail.com>
2026-05-13 14:45:06 +0300

    fix: name change from section to exam

--- database/migrations/2026_05_11_074748_create_topics_table.php
+++ database/migrations/2026_05_11_074748_create_topics_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Exam;
 use App\Models\Section;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -14,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('topics', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Section::class,'section_id')->constrained()->cascadeOnDelete();;
+            $table->foreignIdFor(Exam::class,'exam_id')->constrained()->cascadeOnDelete();;
             $table->string('name');
             $table->string('slug');
             $table->timestamps();
